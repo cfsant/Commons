5e99a6619a8e6ba5d9360cea64d04f8760d4a558
CAIO FISCHER <caiofischer@infosiscs.com>
2021-11-01 14:58:56 -0300

    Add Profile Configuration

---
+++ Commons/Commons/Configurations/ProfileConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Commons.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Commons.Configurations
+{
+    public class ProfileConfiguration : IEntityTypeConfiguration<Profile>
+    {
+        public void Configure(EntityTypeBuilder<Profile> entityTypeBuilder)
+        {
+            entityTypeBuilder.ToTable(nameof(Profile));
+
+            entityTypeBuilder.HasIndex(x => x.Name).IsUnique();
+            entityTypeBuilder.Property(x => x.State).HasDefaultValue(true);
+            entityTypeBuilder.Property(x => x.Insertion).HasDefaultValue(DateTime.Now);
+            entityTypeBuilder.Property(x => x.Change).HasDefaultValue(DateTime.MinValue);
+            entityTypeBuilder.Property(x => x.OwnerId).HasDefaultValue(Guid.Empty);
+            entityTypeBuilder.Property(x => x.PublisherId).HasDefaultValue(Guid.Empty);
+        }
+    }
+}
