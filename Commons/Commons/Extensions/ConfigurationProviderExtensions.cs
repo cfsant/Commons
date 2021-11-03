ad24b83030e1fcabb0a614402346aa73a49fcf41
CAIO SANTANA <cfsant>
2021-11-03 15:21:36 -0300

    Implement ConfigurationProviderExtensions

---
+++ Commons/Commons/Extensions/ConfigurationProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons.Extensions
+{
+    public static class ConfigurationProviderExtensions
+    {
+		public static IServiceCollection AddConfigurationProviderMiddleware(this IServiceCollection services, Type configurationProviderModel)
+		{
+			return services.AddTransient(configurationProviderModel);
+		}
+	}
+}
